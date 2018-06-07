33e0b97f96d7c0cd860bab954e9a6b9a6c42d970
symonayixitielu <zifengwang2016@foxmail.com>
2018-06-07 18:17:59 +0800

    Easy:Array

---
+++ Easy/Array.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# Python 3.6.2 AMD 64
+'''
+Leetcode Primary Algorithm Practices (Array)
+@url: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/
+'''
+
+"1"
+def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i = 0
+    while i < len(nums)-1:
+        if nums[i] == nums[i+1]:
+            nums.remove(nums[i])
+        else:
+            i += 1
+    return len(nums)
+
+"2" 
+def maxProfit(self, prices):
+    
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    max_prof,d = 0,0
+    for i in range(len(prices)):
+        if i > 0:
+            d = prices[i]-prices[i-1]
+        if d>0:
+            max_prof += d
+            
+    return max_prof
+
+"3"    
+def rotate(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    nums[:n-k] = nums[:n-k][::-1]
+    nums[n-k:] = nums[n-k:][::-1]
+    nums.reverse()
+
+"4"    
+def containsDuplicate(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    set_num = set(nums)
+    list_set_num = list(set_num)
+    if len(list_set_num) == len(nums):
+        return False
+    else:
+        return True 
+
+"5"        
+def singleNumber(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    xor = 0
+    for i in range(len(nums)):
+        xor = xor ^ nums[i]
+    return xor        
+    
+"6"
+def intersect(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    """
+    # Pythonic:
+    from collections import Counter
+    nums_intersect = list((Counter(nums1)&Counter(nums2)).elements())
+
+    return nums_intersect
+    """
+    # Normal:
+    if len(nums1) < 1 or len(nums2) < 1:
+        return []
+    res = []
+    counter = {}
+    for num in nums1:
+        counter[num] = counter.get(num,0) + 1     
+    for num in nums2:
+        if counter.get(num) and num in counter:
+            res.append(num)
+            counter[num] -= 1
+    return res        
+
+"7"
+def plusOne(self, digits):
+    """
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    real_num = int("".join([str(n) for n in digits]))
+    real_num += 1
+    new_digits = [int(n) for n in str(real_num)]
+    return new_digits
+    
+"8"
+def moveZeroes(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    num_all = len(nums)
+    num_nonzero = len([n for n in nums if n!=0])
+    num_zero = num_all - num_nonzero
+    for i in range(num_zero):
+        nums.remove(0)
+        
+    nums.extend([0]*num_zero)
+
+
+
+
+    
+        
